056f96459cdde1eca7f1789b40900b5e85fa043f
Platensa02 <platensa02@gmail.com>
2026-02-22 04:44:33 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,25 +1,24 @@
 import asyncpg
 from config import DATABASE_URL
 
-# Database connection pool yaratish
+# Database connection pool
 async def create_pool():
     return await asyncpg.create_pool(DATABASE_URL)
 
-# Jadvallarni avtomatik yaratish
+# Jadval yaratish
 async def init_db(pool):
     async with pool.acquire() as conn:
 
-        # Users jadvali
+        # Users table
         await conn.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             telegram_id BIGINT UNIQUE,
-            full_name TEXT,
-            role TEXT
+            is_active BOOLEAN DEFAULT FALSE
         );
         """)
 
-        # Usage jadvali (ishlatish)
+        # Usage table
         await conn.execute("""
         CREATE TABLE IF NOT EXISTS usage (
             id SERIAL PRIMARY KEY,
@@ -29,7 +28,7 @@ async def init_db(pool):
         );
         """)
 
-        # Payments jadvali (to'lov)
+        # Payments table
         await conn.execute("""
         CREATE TABLE IF NOT EXISTS payments (
             id SERIAL PRIMARY KEY,
