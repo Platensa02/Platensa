ea0c749ebee75ab11bbab32a82448f42f0dd560e
Platensa02 <platensa02@gmail.com>
2026-02-22 04:12:56 +0500

    Implement /start command handler with role-based response
    Added a start handler to respond to the /start command based on user role.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import asyncio
 from aiogram import Bot, Dispatcher
-from config import BOT_TOKEN
-from database import create_pool, init_db
+from aiogram.filters import Command
+from aiogram.types import Message
 
-# Routerlar
+from config import BOT_TOKEN, ADMIN_ID
+from database import create_pool, init_db
 from handlers import admin, client
+from keyboards import admin_menu, client_panel
+
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
@@ -21,9 +24,20 @@ async def main():
     dp.include_router(admin.router)
     dp.include_router(client.router)
 
+    # START handler
+    @dp.message(Command("start"))
+    async def start_handler(message: Message):
+        user_id = message.from_user.id
+
+        if user_id == ADMIN_ID:
+            await message.answer("👨‍💼 Admin panel", reply_markup=admin_menu())
+        else:
+            await message.answer("👤 Mijoz panel", reply_markup=client_panel())
+
     print("🚀 Bot ishga tushdi...")
 
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
