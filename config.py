43897b41091c672b1e8b9e27c1f249ef4c74a11e
Platensa02 <platensa02@gmail.com>
2026-02-22 20:25:25 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
 
-# Telegram Bot Token
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+ADMIN_ID = int(os.environ.get("ADMIN_ID"))
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-# Admin Telegram ID
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN topilmadi")
 
-# Database URL (Render PostgreSQL)
-DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL topilmadi")
