03273e5206c05703b814fc4c05dc79d7c3e6e26c
Platensa02 <platensa02@gmail.com>
2026-02-23 20:57:27 +0500

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,11 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+def admin_menu():
+    return ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text="📦 Mahsulot qo‘shish")],
+            [KeyboardButton(text="💰 To‘lov kiritish")],
+            [KeyboardButton(text="📊 Hisobot")]
+        ],
+        resize_keyboard=True
+    )
