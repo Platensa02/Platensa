870b70668f4379f1724de20a4d58325d05ba1196
Platensa02 <platensa02@gmail.com>
2026-02-23 21:16:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 import os
 import asyncio
-import asyncpg
-
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
+from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from menu import admin_menu
+
+from database import init_db
+import handlers
 
 # =====================
 # ENV VARIABLES
 # =====================
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 # =====================
 # BOT & DP
@@ -21,48 +18,15 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
 
 # =====================
-# START COMMAND
+# HANDLERS ULASH
 # =====================
-@dp.message(Command("start"))
-async def start(message: types.Message):
-
-    user = message.from_user
-
-    # ===== ADMIN =====
-    if user.id == ADMIN_ID:
-        await message.answer("Admin panel:", reply_markup=admin_menu())
-        return
-
-    # ===== CLIENT =====
-    conn = await asyncpg.connect(DATABASE_URL)
-
-    client = await conn.fetchrow(
-        "SELECT * FROM clients WHERE user_id=$1",
-        user.id
-    )
-
-    if not client:
-        await conn.execute(
-            "INSERT INTO clients (user_id, name) VALUES ($1, $2)",
-            user.id,
-            user.full_name
-        )
-
-        await bot.send_message(
-            ADMIN_ID,
-            f"🆕 Yangi mijoz qo‘shildi:\n"
-            f"👤 Ism: {user.full_name}\n"
-            f"🆔 ID: {user.id}"
-        )
-
-    await conn.close()
-
-    await message.answer("Bot ishlayapti ✅")
+handlers.setup(dp, bot)
 
 # =====================
 # MAIN
 # =====================
 async def main():
+    await init_db()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
