5e8c379c4f252baf1d1c44796df822b4b1ebee05
Platensa02 <platensa02@gmail.com>
2026-02-23 20:26:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
-import os
-import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram import types
 from aiogram.filters import Command
 import asyncpg
-from database import init_db
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
+import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-
-async def get_conn():
-    return await asyncpg.connect(DATABASE_URL)
-
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    await message.answer("Bot ishlayapti ✅")
+    user = message.from_user
 
+    conn = await asyncpg.connect(DATABASE_URL)
 
-async def main():
-    await init_db()
-    await dp.start_polling(bot)
+    # user bazada bormi tekshiramiz
+    client = await conn.fetchrow(
+        "SELECT * FROM clients WHERE user_id=$1",
+        user.id
+    )
 
-if __name__ == "__main__":
-    asyncio.run(main())
+    # Agar yo'q bo'lsa qo'shamiz
+    if not client:
+        await conn.execute(
+            "INSERT INTO clients (user_id, name) VALUES ($1, $2)",
+            user.id,
+            user.full_name
+        )
+
+        # Admin ga xabar
+        await bot.send_message(
+            ADMIN_ID,
+            f"🆕 Yangi mijoz qo‘shildi:\n"
+            f"👤 Ism: {user.full_name}\n"
+            f"🆔 ID: {user.id}"
+        )
+
+    await conn.close()
+
+    await message.answer("Botga xush kelibsiz ✅")
