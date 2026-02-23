48b038ab86cd7896cd8bb4a65a6a7b3d146b9187
Platensa02 <platensa02@gmail.com>
2026-02-24 00:02:53 +0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -57,11 +57,30 @@ def setup(dp, bot_instance):
 # =====================
 async def start(message: types.Message):
 
-    if message.from_user.id == ADMIN_ID:
-        await message.answer("Admin panel:", reply_markup=admin_menu())
-    else:
-        await message.answer("Mijoz panel:", reply_markup=client_menu())
+    user = message.from_user
 
+    if user.id == ADMIN_ID:
+        await message.answer("Admin panel:", reply_markup=admin_menu())
+        return
+
+    conn = await asyncpg.connect(DATABASE_URL)
+
+    # 🔎 Bazada bormi tekshiramiz
+    existing = await conn.fetchrow(
+        "SELECT * FROM clients WHERE user_id=$1",
+        user.id
+    )
+
+    # ❗ Agar yo‘q bo‘lsa qo‘shamiz
+    if not existing:
+        await conn.execute("""
+            INSERT INTO clients (user_id, name, confirmed_amount)
+            VALUES ($1, $2, 0)
+        """, user.id, user.full_name)
+
+    await conn.close()
+
+    await message.answer("Mijoz panel:", reply_markup=client_menu())
 
 # =====================
 # ADD PRODUCT START
