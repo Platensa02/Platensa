d1007f88faeddeea3a92248b04c542b80e717c92
Platensa02 <platensa02@gmail.com>
2026-02-23 06:04:07 +0500

    Update reply.py

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,41 +1,19 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-
-# =========================
-# 🔙 ORQAGA TUGMASI
-# =========================
-def back_button():
+def admin_menu():
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="🔙 Orqaga")]
+            [KeyboardButton(text="📋 Mijozlar")],
+            [KeyboardButton(text="➕ Qo‘shish")],
+            [KeyboardButton(text="➖ Yopish")]
         ],
         resize_keyboard=True
     )
 
-
-# =========================
-# 👨‍💼 ADMIN BOSH MENYU
-# =========================
-def admin_menu():
-    return ReplyKeyboardMarkup(
-        keyboard=[
-            [KeyboardButton(text="👥 Mijozlar")],
-            [KeyboardButton(text="➕ Mijoz qo‘shish")]
-        ],
-        resize_keyboard=True,
-        input_field_placeholder="Admin panel"
-    )
-
-
-# =========================
-# 👤 MIJOZ BOSH MENYU
-# =========================
 def client_menu():
     return ReplyKeyboardMarkup(
         keyboard=[
-            [KeyboardButton(text="📦 Jami ishlatganim")],
-            [KeyboardButton(text="📊 Qolganim")]
+            [KeyboardButton(text="📊 Statistika")]
         ],
-        resize_keyboard=True,
-        input_field_placeholder="Mijoz panel"
+        resize_keyboard=True
     )
